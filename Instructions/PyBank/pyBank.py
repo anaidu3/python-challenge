0ef2f15f13953f14416db92bf7db89c9fc04251f
Naidu <Anisha.Naidu@solvay.com>
2022-03-31 20:44:03 -0400

    pyBank.py complete

--- Instructions/PyBank/pyBank.py
+++ Instructions/PyBank/pyBank.py
@@ -22,42 +22,47 @@ with open(csv_path) as csvfile:
         #print(row)
     
     #initialize variables
-    months = 0
-    net_total_amount = 0
+    month_list = []
+    profit_list = []
     profit_change_list = []
     previous_amount = 0
 
     for line in csvreader:
-        months = months + 1
-        net_total_amount = net_total_amount + int(line[1])
-        previous_amount = net_total_amount
-        profit_change_list.append([line[0],previous_amount-int(line+1[1])])
+        month_list.append(line[0])
+        profit_list.append(int(line[1]))
+
+    #number of months
+    months = len(month_list)
+   
+    #net total amount
+    net_total_amount = sum(profit_list)
+
+    #creates a list of the changes each month
+    for i in range(len(profit_list)-1):
+        profit_change_list.append(profit_list[i+1]-profit_list[i])
     
-    print(profit_change_list)
-    #average_profit_change = sum(profit_change_list)/months
+    #average profit change
+    average_profit_change = round(sum(profit_change_list)/(len(profit_list)-1),2)
     
+    #greatest increase
     greatest_increase_amount = max(profit_change_list)
     index_increase_date = profit_change_list.index(greatest_increase_amount)
-    greatest_increase_date = profit_change_list(index_increase_date)
+    greatest_increase_date = month_list[index_increase_date+1]
 
+    #greatest decrease
     greatest_decrease_amount = min(profit_change_list)
-    index_decrease_date = profit_change_list.index(greatest_increase_amount)
-    greatest_decrease_date = profit_change_list(index_decrease_date)
-
+    index_decrease_date = profit_change_list.index(greatest_decrease_amount)
+    greatest_decrease_date = month_list[index_decrease_date+1]
 
     #The total number of months included in the dataset
+    #The net total amount of "Profit/Losses" over the entire period
+    #The changes in "Profit/Losses" over the entire period, and then the average of those changes
+    #The greatest increase in profits (date and amount) over the entire period
+    #The greatest decrease in profits (date and amount) over the entire period 
+
     print(f"Financial Analysis \n---------------------")
     print(f"Total months: {months}") 
     print(f"Total: ${net_total_amount}")
-    print(f'Average change:{average_profit_change}')
-
-
-#change_from_previous.append([row[0], revenue - previous_revenue])
-#maxChangeMonth = dateProfitLossesList[changeList.index(max(changeList))]
-
-#The net total amount of "Profit/Losses" over the entire period
-#The changes in "Profit/Losses" over the entire period, and then the average of those changes
-
-#The greatest increase in profits (date and amount) over the entire period
-
-#The greatest decrease in profits (date and amount) over the entire period
+    print(f'Average change: ${average_profit_change}')
+    print(f'Greatest Increase in Profits: {greatest_increase_date} (${greatest_increase_amount})')
+    print(f'Greatest Decrease in Profits: {greatest_decrease_date} (${greatest_decrease_amount})')
